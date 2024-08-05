274c86fb978d5429d7b0f14fcde4060cc2f5e1c1
Érudit Aoue <113186147+Phiphilio@users.noreply.github.com>
2024-08-05 14:09:03 +0200

    on a un tableau associatif

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-
 $recette = [
     [
         'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
         'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
     ],
     [
         'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
         'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
     ],
     [
         'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
         'author' => 'mathieu.nebra@exemple.com',
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'author' => 'laurene.castor@exemple.com',
+        'is_enabled' => true,
     ],
 ];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,17 +30,15 @@ $recette = [
 </head>
 
 <body>
-    <h2>Page de test</h2>
+    <h2>affichage des recettes</h2>
 
-
-    <ul>
-        <?php for($i = 0; $i <3; $i++ ):?>
-        <li > <?php echo 'le '. $recette[$i]['title'] . ' fait par '.$recette[$i]['author'] ?></li>
-        <?php endfor ?>
-        
-    </ul>
-
-    
+    <?php foreach ($recette as $recetteValue) : ?>
+        <?php if (array_key_exists('is_enabled', $recetteValue) && ($recetteValue['is_enabled'] === true)) : ?>
+            <h2><?php echo $recetteValue['title'] ?></h2>
+            <p><?php echo $recetteValue['recipe'] ?></p>
+            <p><?php echo $recetteValue['author'] ?></p>
+        <?php endif ?>
+    <?php endforeach ?>
 </body>
 
 </html>
